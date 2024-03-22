06d16322e631fc2a4839ae663695a87eedd8f4a6
hendfull1 <iigorvakhrushev@gmail.com>
2024-03-22 21:23:15 +0500

    Убрал тестовые зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.junit.jupiter:junit-jupiter:5.9.1")
-}
-
 tasks.test {
     useJUnitPlatform()
 }
