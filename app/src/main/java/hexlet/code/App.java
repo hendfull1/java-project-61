4d21c3cb10a2661db57acfcc2c3707d1097b7254
hendfull1 <iigorvakhrushev@gmail.com>
2024-03-25 15:07:57 +0500

    Исправил класс APP

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,9 @@ public class App {
     private static final int PROGRESSION = 5;
     private static final int PRIME = 6;
 
-    public static void choice() {
+
+    public static void main(String[] args) {
+        Scanner game = new Scanner(System.in);
         System.out.println("Please enter the game and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
@@ -18,32 +20,28 @@ public class App {
         System.out.println("5 - Progression");
         System.out.println("6 - Prime");
         System.out.println("0 - Exit");
-    }
-
-    public static void main(String[] args) {
-        Scanner game = new Scanner(System.in);
-        choice();
         int numberGame = game.nextInt();
         System.out.println("Your choice: " + numberGame);
-        if (numberGame == GREET) {
-            System.out.println("Welcome to the Brain Games!");
-            // Оставил Cli только тут
-            Cli.cli();
-        }
-        if (numberGame == EVEN) {
-            IsEven.gameIsEven();
-        }
-        if (numberGame == CALC) {
-            Calc.gameCalc();
-        }
-        if (numberGame == GCD) {
-            Gcd.gameGcd();
-        }
-        if (numberGame == PROGRESSION) {
-            Progression.gameProgression();
-        }
-        if (numberGame == PRIME) {
-            IsPrime.gamePrime();
+        switch (numberGame) {
+            case GREET:
+                System.out.println("Welcome to the Brain Games!");
+                Cli.cli();
+                break;
+            case EVEN:
+                IsEven.gameIsEven();
+                break;
+            case CALC:
+                Calc.gameCalc();
+                break;
+            case GCD:
+                Gcd.gameGcd();
+                break;
+            case PROGRESSION:
+                Progression.gameProgression();
+                break;
+            case PRIME:
+                IsPrime.gamePrime();
+                break;
         }
     }
 }
