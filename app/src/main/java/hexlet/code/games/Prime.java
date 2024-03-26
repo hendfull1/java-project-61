ad8557df979bd98ead0d1c35c547a5aab8b5e9fc
hendfull1 <iigorvakhrushev@gmail.com>
2024-03-26 23:54:44 +0500

    Исправил метод prime

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -8,19 +8,26 @@ public class Prime {
     public static String getQuestion() {
         return "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
     }
-    public static String isPrime(int number) {
+    public static Boolean isPrime(int number) {
+        if (number <= 1) {
+            return Boolean.FALSE;
+        }
         for (var i = 2; i < number; i++) {
             if (number % i == 0) {
-                return "no";
+                return Boolean.FALSE;
             }
         }
-        return "yes";
+        return Boolean.TRUE;
     }
     public static void gamePrime() {
         String[][] numbers = new String[Engine.GAME_LENGTH][2];
         for (var number : numbers) {
             number[0] = Integer.toString(Utils.randomNumber(Utils.MIN_GENERATE, Utils.MAX_GENERATE));
-            number[1] = isPrime(Integer.parseInt(number[0]));
+            if (isPrime(Integer.parseInt(number[0])) == Boolean.TRUE) {
+                number[1] = "yes";
+            } else {
+                number[1] = "no";
+            }
         }
         Engine.game(numbers, getQuestion());
     }
